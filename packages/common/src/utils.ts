97a3d75f10c0ca517a808672cab83a55a0e51475
yiiouo <15625979610@163.com>
2024-01-02 10:40:45 +0800

    fix: 修正 safari 判断

--- packages/common/src/utils.ts
+++ packages/common/src/utils.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Lu
  * @Date: 2023-11-09 16:41:14
- * @LastEditTime: 2024-01-02 00:51:24
+ * @LastEditTime: 2024-01-02 10:39:49
  * @LastEditors: Lu
  * @Description: 
  */
@@ -15,17 +15,22 @@ export const log = (...args: unknown[]) => isLog ? window['console'].log(...args
 
 export const getEmitter = () => emitter
 
-export const getBrowserInfo = () => ({
-  isAndroid: Boolean(navigator.userAgent.match(/android/ig)),
-  isIpad: Boolean(navigator.userAgent.match(/ipad/ig)),
-  isIos: Boolean(navigator.userAgent.match(/ipad|iphone/ig)),
-  isSafari: (/CriOS/i.test(navigator.userAgent) && /iphone|ipod|ipad/i.test(navigator.userAgent)) || /Safari/i.test(navigator.userAgent),
-  isWeixin: Boolean(navigator.userAgent.match(/MicroMessenger/ig)),
-  isAli: Boolean(navigator.userAgent.match(/AlipayClient/ig)),
-  isPhone: Boolean(/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)),
-  isBaidu: (navigator.userAgent.includes('baiduboxapp') || navigator.userAgent.includes('Baidu')),
-  isQuark: navigator.userAgent.includes('Quark'),
-  isUC: navigator.userAgent.includes('UCBrowser'),
-  isQQ: navigator.userAgent.includes('MQQBrowser')
-})
+export const getBrowserInfo = () => {
+  const obj = {
+    isAndroid: Boolean(navigator.userAgent.match(/android/ig)),
+    isIpad: Boolean(navigator.userAgent.match(/ipad/ig)),
+    isIos: Boolean(navigator.userAgent.match(/ipad|iphone/ig)),
+    isWeixin: Boolean(navigator.userAgent.match(/MicroMessenger/ig)),
+    isAli: Boolean(navigator.userAgent.match(/AlipayClient/ig)),
+    isPhone: Boolean(/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)),
+    isBaidu: (navigator.userAgent.includes('baiduboxapp') || navigator.userAgent.includes('Baidu')),
+    isQuark: navigator.userAgent.includes('Quark'),
+    isUC: navigator.userAgent.includes('UCBrowser'),
+    isQQ: navigator.userAgent.includes('MQQBrowser')
+  }
+  return {
+    ...obj,
+    isSafari: !obj.isAndroid && ((/CriOS/i.test(navigator.userAgent) && /iphone|ipod|ipad/i.test(navigator.userAgent)) || /Safari/i.test(navigator.userAgent))
+  }
+}
 
